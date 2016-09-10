2fdf369b797bc5390eb547474e334c93594f5597
TsarAl <alancarregal@gmail.com>
2016-09-10 16:45:34 -0400

    Final Programming Assignment 2 for R Programming on Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Creates a Matrix with the Inverse being able to be
+# set and cached
+makeCacheMatrix <- function(mX = matrix()) 
+{
+        invX <- NULL
+        set  <- function(y) 
+		{
+			mX <<- y
+	                invX <<- NULL
+		}
 
-## Write a short comment describing this function
+        get  <- function() 
+		{
+			mX
+		}
 
-makeCacheMatrix <- function(x = matrix()) {
+        setInverse <- function(inverse) 
+		      {
+			invX <<- inverse
+		      }
 
+        getInverse <- function() 
+		      {
+			invX
+		      }
+
+        list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Solve for the inverse of a matrix and set the inverse to
+# be cached
+cacheSolve <- function(mX, ..., verbose=TRUE) 
+{
+        ## Return a matrix that is the inverse of 'mX'
+	if(class(mX)=='list') 
+	{
+	        invX <- mX$getInverse()
+	        if (!is.null(invX)) 
+		{
+	                if (verbose==TRUE) message("Getting cached data")
+		} else
+		{
+			mY <- mX$get()
+		        invX <- solve(mY, ...)
+		        mX$setInverse(invX)
+		}
+	} else # if class other than list is passed then just solve with no cache
+	{
+		if (verbose==TRUE) message(paste("Solving for class",class(mX)))
+		invX <- solve(mX, ...)
+	}
+        
+	if (verbose==TRUE) return(invX)
 }
+
